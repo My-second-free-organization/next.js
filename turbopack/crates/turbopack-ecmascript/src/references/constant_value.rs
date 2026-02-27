c9e259a5292a0efc7467f750754a10de6b116892
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-27 14:33:49 +0100

    rework constant_value ConstantValueCodeGen

--- turbopack/crates/turbopack-ecmascript/src/references/constant_value.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/constant_value.rs
@@ -1,9 +1,14 @@
+use std::borrow::Cow;
+
 use anyhow::Result;
 use bincode::{Decode, Encode};
+use either::Either;
 use swc_core::{
     common::{DUMMY_SP, FileName, SourceMap, sync::Lrc},
     ecma::{
-        ast::{ArrayLit, EsVersion, Expr, KeyValueProp, ObjectLit, Prop, PropName, Str},
+        ast::{
+            ArrayLit, EsVersion, Expr, KeyValueProp, Lit, ObjectLit, Prop, PropName, PropOrSpread,
+        },
         parser::{Syntax, parse_file_as_expr},
     },
     quote,
@@ -13,7 +18,7 @@ use turbo_tasks::{NonLocalValue, Vc, debug::ValueDebugFormat, trace::TraceRawVcs
 use turbopack_core::{chunk::ChunkingContext, compile_time_info::CompileTimeDefineValue};
 
 use crate::{
-    analyzer::ConstantValue,
+    analyzer::{ConstantValue, JsValue, ObjectPart},
     code_gen::{CodeGen, CodeGeneration},
     create_visitor,
     references::AstPath,
@@ -22,28 +27,29 @@ use crate::{
 #[derive(
     Clone, Debug, PartialEq, Eq, Hash, TraceRawVcs, ValueDebugFormat, NonLocalValue, Encode, Decode,
 )]
+enum JsValueOrParsableExpr {
+    JsValue(JsValue),
+    ParsableExpr(RcStr),
+}
+
+#[derive(
+    Clone, Debug, PartialEq, Eq, Hash, TraceRawVcs, ValueDebugFormat, NonLocalValue, Encode, Decode,
+)]
 pub struct ConstantValueCodeGen {
-    value: CompileTimeDefineValue,
+    value: JsValueOrParsableExpr,
     path: AstPath,
 }
 
 impl ConstantValueCodeGen {
     pub fn new(value: CompileTimeDefineValue, path: AstPath) -> Self {
-        ConstantValueCodeGen { value, path }
+        ConstantValueCodeGen {
+            value: compile_time_define_to_js_value(value),
+            path,
+        }
     }
     pub fn new_jsvalue(value: ConstantValue, path: AstPath) -> Self {
         ConstantValueCodeGen {
-            // TODO do this properly
-            value: match value {
-                ConstantValue::Undefined => CompileTimeDefineValue::Undefined,
-                ConstantValue::Null => CompileTimeDefineValue::Null,
-                ConstantValue::Str(v) => CompileTimeDefineValue::String(v.as_rcstr()),
-                ConstantValue::Num(v) => CompileTimeDefineValue::Number(v.0.to_string().into()),
-                ConstantValue::True => CompileTimeDefineValue::Bool(true),
-                ConstantValue::False => CompileTimeDefineValue::Bool(false),
-                ConstantValue::BigInt(_) => unimplemented!("ConstantValueCodeGen: Bigint"),
-                ConstantValue::Regex(_) => unimplemented!("ConstantValueCodeGen: Regex"),
-            },
+            value: JsValueOrParsableExpr::JsValue(JsValue::Constant(value)),
             path,
         }
     }
@@ -54,8 +60,10 @@ impl ConstantValueCodeGen {
         let value = self.value.clone();
 
         let visitor = create_visitor!(self.path, visit_mut_expr, |expr: &mut Expr| {
-            // TODO: avoid this clone
-            *expr = define_env_to_expr((value).clone());
+            *expr = js_value_to_expr(match &value {
+                JsValueOrParsableExpr::JsValue(js_value) => Either::Left(Cow::Borrowed(js_value)),
+                JsValueOrParsableExpr::ParsableExpr(s) => Either::Right(s),
+            });
         });
 
         Ok(CodeGeneration::visitors(vec![visitor]))
@@ -68,54 +76,126 @@ impl From<ConstantValueCodeGen> for CodeGen {
     }
 }
 
-fn define_env_to_expr(value: CompileTimeDefineValue) -> Expr {
+fn compile_time_define_to_js_value(value: CompileTimeDefineValue) -> JsValueOrParsableExpr {
+    JsValueOrParsableExpr::JsValue(match value {
+        CompileTimeDefineValue::Null => JsValue::Constant(ConstantValue::Null),
+        CompileTimeDefineValue::Undefined => JsValue::Constant(ConstantValue::Undefined),
+        CompileTimeDefineValue::Bool(false) => JsValue::Constant(ConstantValue::False),
+        CompileTimeDefineValue::Bool(true) => JsValue::Constant(ConstantValue::True),
+        CompileTimeDefineValue::Number(s) => {
+            JsValue::Constant(ConstantValue::Num(s.parse::<f64>().unwrap().into()))
+        }
+        CompileTimeDefineValue::String(s) => JsValue::Constant(ConstantValue::Str(s.into())),
+        CompileTimeDefineValue::Array(items) => JsValue::frozen_array(
+            items
+                .into_iter()
+                .map(compile_time_define_to_js_value)
+                .map(|v| match v {
+                    JsValueOrParsableExpr::JsValue(js_value) => js_value,
+                    JsValueOrParsableExpr::ParsableExpr(_) => panic!(
+                        "unexpected parsable expr in compile-time define array value: {:?}",
+                        v
+                    ),
+                })
+                .collect(),
+        ),
+        CompileTimeDefineValue::Object(items) => JsValue::frozen_object(
+            items
+                .into_iter()
+                .map(|(k, v)| {
+                    let value = compile_time_define_to_js_value(v);
+                    ObjectPart::KeyValue(
+                        k.into(),
+                        match value {
+                            JsValueOrParsableExpr::JsValue(js_value) => js_value,
+                            JsValueOrParsableExpr::ParsableExpr(_) => panic!(
+                                "unexpected parsable expr in compile-time define object value: \
+                                 {:?}",
+                                value
+                            ),
+                        },
+                    )
+                })
+                .collect(),
+        ),
+        CompileTimeDefineValue::Evaluate(s) => {
+            return JsValueOrParsableExpr::ParsableExpr(s);
+        }
+    })
+}
+
+fn js_value_to_expr(value: Either<Cow<'_, JsValue>, &RcStr>) -> Expr {
     match value {
-        CompileTimeDefineValue::Null => {
-            quote!("(\"TURBOPACK compile-time value\", null)" as Expr)
-        }
-        CompileTimeDefineValue::Bool(true) => {
-            quote!("(\"TURBOPACK compile-time value\", true)" as Expr)
-        }
-        CompileTimeDefineValue::Bool(false) => {
-            quote!("(\"TURBOPACK compile-time value\", false)" as Expr)
-        }
-        CompileTimeDefineValue::Number(ref n) => {
-            quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = n.parse::<f64>().unwrap().into())
-        }
-        CompileTimeDefineValue::String(ref s) => {
-            quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = s.to_string().into())
-        }
-        CompileTimeDefineValue::Array(a) => {
-            quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = Expr::Array(ArrayLit {
-                span: DUMMY_SP,
-                elems: a.into_iter().map(|i| Some(define_env_to_expr(i).into())).collect(),
-            }))
-        }
-        CompileTimeDefineValue::Object(m) => {
-            quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = Expr::Object(ObjectLit {
-                span: DUMMY_SP,
-                props: m
-                    .into_iter()
-                    .map(|(k, v)| {
-                        swc_core::ecma::ast::PropOrSpread::Prop(
-                            Prop::KeyValue(KeyValueProp {
-                                key: PropName::Str(Str::from(k.as_str())),
-                                value: define_env_to_expr(v).into(),
-                            })
-                            .into(),
-                        )
-                    })
-                    .collect(),
-            }))
-        }
-        CompileTimeDefineValue::Undefined => {
-            quote!("(\"TURBOPACK compile-time value\", void 0)" as Expr)
-        }
-        CompileTimeDefineValue::Evaluate(ref s) => parse_single_expr_lit(s.clone()),
+        Either::Left(value) => match &*value {
+            JsValue::Constant(ConstantValue::Undefined) => {
+                quote!("(\"TURBOPACK compile-time value\", void 0)" as Expr)
+            }
+            JsValue::Constant(ConstantValue::Null) => {
+                quote!("(\"TURBOPACK compile-time value\", null)" as Expr)
+            }
+            JsValue::Constant(ConstantValue::True) => {
+                quote!("(\"TURBOPACK compile-time value\", true)" as Expr)
+            }
+            JsValue::Constant(ConstantValue::False) => {
+                quote!("(\"TURBOPACK compile-time value\", false)" as Expr)
+            }
+            JsValue::Constant(ConstantValue::Num(n)) => {
+                quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = n.0.into())
+            }
+            JsValue::Constant(ConstantValue::Str(s)) => {
+                quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = s.as_str().into())
+            }
+            JsValue::Array { items, .. } => {
+                quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = Expr::Array(ArrayLit {
+                    span: DUMMY_SP,
+                    elems: items.iter().map(|i| Some(js_value_to_expr(Either::Left(Cow::Borrowed(i))).into())).collect(),
+                }))
+            }
+            JsValue::Object { parts, .. } => {
+                let e = Expr::Object(ObjectLit {
+                    span: DUMMY_SP,
+                    props: parts
+                        .iter()
+                        .map(|p| match p {
+                            ObjectPart::KeyValue(k, v) => PropOrSpread::Prop(
+                                Prop::KeyValue(KeyValueProp {
+                                    key: match js_value_to_expr(Either::Left(Cow::Borrowed(k))) {
+                                        Expr::Lit(Lit::Str(s)) => PropName::Str(s),
+                                        Expr::Lit(Lit::Num(n)) => PropName::Num(n.into()),
+                                        _ => panic!(
+                                            "unexpected value for compile-time define object key: \
+                                             {}",
+                                            k
+                                        ),
+                                    },
+                                    value: js_value_to_expr(Either::Left(Cow::Borrowed(v))).into(),
+                                })
+                                .into(),
+                            ),
+                            ObjectPart::Spread(_) => {
+                                panic!(
+                                    "unexpected spread variant for compile-time define value: {}",
+                                    value
+                                );
+                            }
+                        })
+                        .collect(),
+                });
+                quote!("(\"TURBOPACK compile-time value\", $e)" as Expr, e: Expr = e)
+            }
+            _ => {
+                panic!(
+                    "unexpected JsValue variant for compile-time define value: {}",
+                    value
+                );
+            }
+        },
+
+        Either::Right(s) => parse_single_expr_lit(s),
     }
 }
 
-pub(crate) fn parse_single_expr_lit(expr_lit: RcStr) -> Expr {
+pub(crate) fn parse_single_expr_lit(expr_lit: &RcStr) -> Expr {
     let cm = Lrc::new(SourceMap::default());
     let fm = cm.new_source_file(FileName::Anon.into(), expr_lit.clone());
     parse_file_as_expr(
